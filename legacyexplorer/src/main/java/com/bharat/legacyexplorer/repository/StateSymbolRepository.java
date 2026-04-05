7c7b091e502d4717aba594c7d4222e558bbddc23
Harsh-YDV1 <harshyk2003officialhp@gmail.com>
2026-04-06 01:29:59 +0530

    Update StateSymbolRepository.java

--- legacyexplorer/src/main/java/com/bharat/legacyexplorer/repository/StateSymbolRepository.java
+++ legacyexplorer/src/main/java/com/bharat/legacyexplorer/repository/StateSymbolRepository.java
@@ -1,6 +1,7 @@
 package com.bharat.legacyexplorer.repository;
 
 import com.bharat.legacyexplorer.model.StateSymbol;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StateSymbolRepository extends JpaRepository<StateSymbol, Long> {}
